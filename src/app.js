4b19058b2533723421df6bdc80125d13b8fcc6a2
Nagy <janos@zold.lan>
2022-05-05 08:21:25 +0200

    create, read kész

---
+++ src/app.js
@@ -0,0 +1,79 @@
+
+const url = 'http://localhost:3000/employees';
+const addButton = document.querySelector('#addButton');
+const nameElem = document.querySelector('#name');
+const cityElem = document.querySelector('#city');
+const salaryElem = document.querySelector('#salary');
+const tableBody = document.querySelector('#tableBody');
+
+addButton.addEventListener('click', () => {
+    const name = nameElem.value;
+    const city = cityElem.value;
+    const salary = salaryElem.value;
+    addEmployee(name, city, salary);
+});
+
+getEmployees();
+
+function getEmployees() {
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+        // console.log(res);
+        renderEmployees(res);
+    });
+}
+
+function renderEmployees(employees) {
+    employees.forEach( employee => {
+        let tr = document.createElement('tr');
+        let tdId = document.createElement('td');
+        let tdName = document.createElement('td');
+        let tdCity = document.createElement('td');
+        let tdSalary = document.createElement('td');
+
+        let tdDelete = document.createElement('td');
+        let delButton = document.createElement('button');
+        delButton.textContent = 'Törlés';
+        setEvent(delButton, employee.id);
+        tdDelete.appendChild(delButton);
+
+        tableBody.appendChild(tr);
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdCity);
+        tr.appendChild(tdSalary);
+        tr.appendChild(tdDelete);
+        tdId.textContent = employee.id;
+        tdName.textContent = employee.name;
+        tdCity.textContent = employee.city;
+        tdSalary.textContent = employee.salary;
+        console.log(employee.city);
+    });
+}
+
+function setEvent(delButton, id) {
+    // delButton.setAttribute('data-id', id);
+    delButton.addEventListener('click', () => {
+        // console.log(delButton.dataset.id);
+        console.log(id);
+    });
+}
+
+function addEmployee(name, city, salary) {
+    fetch(url, {
+        method: "post",
+        body: JSON.stringify({
+            name: name,
+            city: city,
+            salary: salary
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+    });
+}
